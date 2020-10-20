57be0b79e2ef873e39ae21f40b07855bbeab3c50
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-10-20 11:51:25 +0200

    add render product

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+const { utils } = require("stylelint");
+
 {
   'use strict';
 
@@ -53,14 +55,45 @@
   };
 
   class Product {
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
 
       console.log('new Product:', thisProduct);
     }
+    renderInMenu(){
+      const thisProduct = this;
+    
+    /* generate HTML based on template */
+    const generatedHTML = templates.menuProduct(thisProduct.data);
+    /* create element using utils.createElementFromHTML */
+    thisProduct element = utils.createDOMFromHTML(this.generatedHTML);
+    /* find menu container */
+    const menuContainer = document.querySelector(select.containerOf.menu);
+    /* add element to menu */
+    menuContainer.appendChild(thisProduct.element);
   }
-
+  }
+  console.log(Product);
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -68,8 +101,15 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
+
+  app.initData();
+
+  app.initMenu();
 
   app.init();
 }
